1593bc262b4d10c5eb732dfa890d75184247c0ea
titoparra4 <tito.parra4@hotmail.com>
2018-09-06 02:40:15 -0500

    Products refreshing

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -5,11 +5,14 @@
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
     using Xamarin.Forms;
 
     public class ProductsViewModel : BaseViewModel
     {
         private ApiService apiService;
+
+        private bool isRefreshing;
 
         private ObservableCollection<Product> products;
         public ObservableCollection<Product> Products
@@ -18,6 +21,11 @@
             set { this.SetValue(ref this.products, value); }
         }
 
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
         public ProductsViewModel()
         {
             this.apiService = new ApiService();
@@ -26,15 +34,20 @@
 
         private async void LoadProducts()
         {
+            this.IsRefreshing = true;
             var response = await this.apiService.GetList<Product>("http://salesapiti.azurewebsites.net", "/api", "/Products");
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
 
             var list = (List<Product>)response.Result;
             this.Products = new ObservableCollection<Product>(list);
+            this.IsRefreshing = false;
         }
+
+
     }
 }
