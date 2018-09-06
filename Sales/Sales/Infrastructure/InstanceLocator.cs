0d1445a31adf52699c20c73e9e27d339edf74bd1
titoparra4 <tito.parra4@hotmail.com>
2018-09-06 00:54:18 -0500

    Product

---
+++ Sales/Sales/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,13 @@
+﻿namespace Sales.Infrastructure
+{
+    using Sales.ViewModels;
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,40 @@
+﻿namespace Sales.ViewModels
+{
+    using Sales.Common.Models;
+    using Sales.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
+
+    public class ProductsViewModel : BaseViewModel
+    {
+        private ApiService apiService;
+
+        private ObservableCollection<Product> products;
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await this.apiService.GetList<Product>("http://salesapiti.azurewebsites.net", "/api", "/Products");
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+
+            var list = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(list);
+        }
+    }
+}
