2d4d0356cd1fc54db403e01990b7bb28878dd699
titoparra4 <tito.parra4@hotmail.com>
2018-09-11 14:52:49 -0500

    AddProducts

--- Sales/Sales/ViewModels/MainViewModel.cs
+++ Sales/Sales/ViewModels/MainViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using Sales.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Sales.ViewModels
 {
@@ -11,6 +15,19 @@ namespace Sales.ViewModels
         public MainViewModel()
         {
             this.Products = new ProductsViewModel();
+        }
+
+        public ICommand AddProductCommand
+        {
+            get
+            {
+                return new RelayCommand(GoToAddProduct);
+            }
+        }
+
+        private async void GoToAddProduct()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new AddProductPage());
         }
     }
 }
